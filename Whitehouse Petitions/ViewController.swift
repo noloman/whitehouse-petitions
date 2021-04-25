f8ec1226dd9a8115b19ea0a56c22f296b0dee80d
Manuel Lorenzo <manulorenzop@gmail.com>
2021-04-25 13:42:05 +0200

    perform tab bar item actions in the main thread

--- Whitehouse Petitions/ViewController.swift
+++ Whitehouse Petitions/ViewController.swift
@@ -9,16 +9,10 @@ import UIKit
 
 class ViewController: UITableViewController {
     var petitions = [Petition]()
+    var urlString: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        performSelector(inBackground: #selector(fetchJson), with: nil)
-    }
-    
-    @objc func fetchJson() {
-        let urlString: String
-        
         if navigationController?.tabBarItem.tag == 0 {
             // urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
             urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
@@ -26,8 +20,11 @@ class ViewController: UITableViewController {
             // urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
             urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
         }
+        performSelector(inBackground: #selector(fetchJson), with: nil)
+    }
     
-        if let url = URL(string: urlString) {
+    @objc func fetchJson() {
+        if let url = URL(string: urlString ?? "") {
             if let data = try? Data(contentsOf: url) {
                 // OK to parse
                 parse(json: data)
